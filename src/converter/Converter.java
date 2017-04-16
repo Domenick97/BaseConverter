794adec07cc032bb96e207265db6b04306af5d03
dndibias <dndibias@ncsu.edu>
2017-04-16 12:25:04 -0400

    Finished the conversion from base 10 to another

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -11,13 +11,13 @@ public class Converter {
 	 *            The current base of the number
 	 * @param iBase
 	 *            The indented base of the number
-	 * @return Integer equivalent to the number passed through but of the
+	 * @return String equivalent to the number passed through but of the
 	 *         intended base
 	 */
-	public static int convert(int num, int cBase, int iBase) {
+	public static String convert(int num, int cBase, int iBase) {
 		if (cBase <= 0 || iBase <= 0)
 			throw new IllegalArgumentException();
-		int newNum;
+		String newNum;
 		if (cBase == 10)
 			newNum = convertDecToBase(num, iBase);
 		else
@@ -36,20 +36,17 @@ public class Converter {
 	 *            The base of the number being passed through
 	 * @return Integer of base 10
 	 */
-	private static int convertToDec(int num, int cBase) {
-		String oldNumS = "";
-		int temp = 0;
+	public static int convertToDec(int num, int cBase) {
+		String oldNumS = "" + num;
 
-		int count = 0;
-		while (temp <= num) {
-			temp = (int) Math.pow(cBase, count);
-			count++;
-		}
-
+		int num1 = 0;
 		for (int i = 0; i < oldNumS.length(); i++) {
-
+			num1 = Character.getNumericValue(oldNumS.charAt(oldNumS.length() - 1 - i));
+			if (num1 > cBase)
+				throw new IllegalArgumentException();
 		}
-		return num;
+
+		return num1;
 	}
 
 	/**
@@ -59,10 +56,49 @@ public class Converter {
 	 *            The number being converted
 	 * @param base
 	 *            The intended base of the number
-	 * @return Integer equivalent to the number passed through but of the
+	 * @return String equivalent to the number passed through but of the
 	 *         intended base
 	 */
-	private static int convertDecToBase(int num, int ibase) {
-		return num;
+	public static String convertDecToBase(int num, int iBase) {
+		if(iBase > 35 || iBase < 2)
+			throw new IllegalArgumentException("Cannot compute the base of a number with the base of " + iBase);
+		
+		int remainder;
+		int number = num;
+		String newNum = "";
+
+		while (number >= iBase) {
+			remainder = number % iBase;
+			number = number / iBase;
+			
+			if(remainder > 9){
+				remainder -= 9;
+				if(remainder > 26)
+					throw new IllegalArgumentException("The base cannot exceed 35");
+				
+				newNum += (char)(remainder + 64);
+			} else{
+				newNum += remainder;
+			}
+		}
+		if(number > 9){
+			number -= 9;
+			if(number > 26)
+				throw new IllegalArgumentException("The base cannot exceed 35");
+			
+			newNum += (char)(number  + 64);
+		} else{
+			newNum += number;
+		}
+		
+		return mirror(newNum);
+	}
+	
+	public static String mirror(String flip){
+		String flipped = "";
+		for(int i = flip.length() - 1; i >= 0; i--){
+			flipped += flip.charAt(i);
+		}
+		return flipped;
 	}
 }
--- test/converter/ConverterTest.java
+++ test/converter/ConverterTest.java
@@ -5,15 +5,56 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * The test class for the Converter class
+ * @author Domenick
+ *
+ */
 public class ConverterTest {
 
 	@Before
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * Test the convert method
+	 */
 	@Test
 	public void testConvert() {
-		assertEquals(1,Converter.convert(15, 10, 2));
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test the convert to decimal method
+	 */
+	@Test
+	public void testConvertToDec() {
+		assertEquals(3, Converter.convertToDec(367, 18));
+	}
+
+	/**
+	 * Test the convert dec to base method
+	 */
+	@Test
+	public void testConvertDecToBase() {
+		assertEquals("10001", Converter.convertDecToBase(17, 2));
+		assertEquals("15D", Converter.convertDecToBase(349, 16));
+		
+		//Try to convert base 10 to base 0
+		try{
+			Converter.convertDecToBase(349, 0);
+			fail();
+		} catch(IllegalArgumentException e){
+			assertEquals("Cannot compute the base of a number with the base of 0", e.getMessage());
+		}
+		
+		//Try to convert base 10 to base 36
+		try{
+			Converter.convertDecToBase(349, 36);
+			fail();
+		} catch(IllegalArgumentException e){
+			assertEquals("Cannot compute the base of a number with the base of 36", e.getMessage());
+		}
 	}
 
 }
